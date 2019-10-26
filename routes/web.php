34aef49297f4bde00b5d4f51a5a83e5c08021b37
rc <3207376143@qq.com>
2019-10-26 15:03:59 +0800

    客户服务

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//服务管理
+Route::get('serve/create','Admin\ServeController@create');
+Route::post('serve/create_do','Admin\ServeController@create_do');
+Route::get('serve/index','Admin\ServeController@index');
+//删除
+Route::get('serve/del/{id}','Admin\ServeController@del');
+//修改视图
+Route::get('serve/edit/{id}','Admin\ServeController@edit');
+//修改执行
+Route::post('serve/update/{id}','Admin\ServeController@update');
